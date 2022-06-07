ef072ede6a207012ce81698350548acc95d4df34
Jyothi <mejyothi33@gmail.com>
2022-06-07 12:28:35 +1000

    added waits and clean-up of the code

--- jbhifi-app-automation/Global/Base.cs
+++ jbhifi-app-automation/Global/Base.cs
@@ -1,70 +1,35 @@
-﻿
-using MarsFramework.Pages;
+﻿using jbhifi_app_automation.Pages;
 using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
+using static jbhifi_app_automation.Global.GlobalDefinitions;
 
-using System;
-using static MarsFramework.Global.GlobalDefinitions;
-
-namespace MarsFramework.Global
+namespace jbhifi_app_automation.Global
 {
     class Base
     {
-        #region To access Path from resource file
-        public static string Url = "https://www.jbhifi.com.au/";
-
+        #region To access URLs
+        public static string URL = "https://www.jbhifi.com.au/";
         #endregion
-
 
 
         #region setup and tear down
         [SetUp]
         public void Initialize()
         {
-
-           driver = new ChromeDriver();
-
-           driver.Manage().Window.Maximize();
-
-
-
-            #region Initialise Reports
-
-
-            /*extent = new ExtentReports(ReportPath, false, DisplayOrder.NewestFirst);
-            extent.LoadConfig(MarsResource.ReportXMLPath);
-
-
-            test = extent.StartTest("report");*/
-
-
-
-            #endregion
-
-
-            SignIn signInlObj = new SignIn();
-
-            signInlObj.LoginSteps();
-
-
-
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            NavigateUrl();
+            ImplicitWaitTime(20);
+            Login loginObj = new Login();
+            loginObj.LoginSteps();
         }
-
 
         [TearDown]
         public void TearDown()
         {
-            //Screenshot
-           /* String img = SaveScreenShotClass.SaveScreenshot(GlobalDefinitions.driver, "Report");//AddScreenCapture(@"E:\Dropbox\VisualStudio\Projects\Beehive\TestReports\ScreenShots\");
-            test.Log(LogStatus.Info, "Image location: " + img);
-            // end test. (Reports)
-            extent.EndTest(test);
-            // calling Flush writes everything to the log file (Reports)
-            extent.Flush();*/
-            // Close the driver :)            
-          GlobalDefinitions.driver.Close();
-           // GlobalDefinitions.driver.Quit();
+
+            GlobalDefinitions.driver.Close();
+
         }
         #endregion
 
